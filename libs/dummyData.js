139c0ced074c1b93ad05b55f9241309a7f7e3f45
thuannguyen88 <getaholdofthuan@gmail.com>
2022-02-18 14:02:40 +0000

    adding dummy data

---
+++ libs/dummyData.js
@@ -0,0 +1,11 @@
+const users = [{
+   id: 1,
+   firstName: "Jane",
+   lastName: "Wilkins",
+   email: "jane.wilkins@gmail.com",
+   address: {street: "Main Street", city: "Ambleside", postcode: "LA22 9BU", country: "United Kingdom"},
+   isActive: null,
+   profilePicture: "",
+   userBio: "hello I'm Jane, i like food",
+   
+}]
