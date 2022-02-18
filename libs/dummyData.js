b49d6bf2d743b6ba9d943acc236f3a86eabbbf04
thuannguyen88 <getaholdofthuan@gmail.com>
2022-02-18 14:12:35 +0000

    user dummy data finished

--- libs/dummyData.js
+++ libs/dummyData.js
@@ -1,11 +1,32 @@
-const users = [{
-   id: 1,
-   firstName: "Jane",
-   lastName: "Wilkins",
-   email: "jane.wilkins@gmail.com",
-   address: {street: "Main Street", city: "Ambleside", postcode: "LA22 9BU", country: "United Kingdom"},
-   isActive: null,
-   profilePicture: "",
-   userBio: "hello I'm Jane, i like food",
-   
-}]
+const users = [
+  {
+    id: 1,
+    firstName: "Jane",
+    lastName: "Wilkins",
+    email: "jane.wilkins@gmail.com",
+    address: {
+      street: "Main Street",
+      city: "Ambleside",
+      postcode: "LA22 9BU",
+      country: "United Kingdom",
+    },
+    isActive: false,
+    profilePicture: "",
+    userBio: "hello I'm Jane, i like food",
+  },
+  {
+    id: 2,
+    firstName: "Dennis",
+    lastName: "Thompson",
+    email: "dennis.thompson@gmail.com",
+    address: {
+      street: "Newlands Meadow",
+      city: "High Wycombe",
+      postcode: "HP11 2BZ",
+      country: "United Kingdom",
+    },
+    isActive: true,
+    profilePicture: "",
+    userBio: "hello I'm Dennis, i like drink",
+  },
+];
