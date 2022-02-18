1b414e3935c3751a514160c71acfa394eea3ad52
Simren Chandan <simrenkaursoor@gmail.com>
2022-02-18 15:47:09 +0000

    created tables for users and items

--- db/connection.js
+++ db/connection.js
@@ -3,10 +3,12 @@ import pg from "pg";
 import { connectionString } from "../config.js";
 
 const pool = new pg.Pool({
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false
-  }
+   connectionString,
+   ssl: {
+      rejectUnauthorized: false,
+   },
 });
 
-export default pool;
+export async function query(text, params) {
+   return pool.query(text, params);
+}
--- db/scripts/users/createTable.js
+++ db/scripts/users/createTable.js
@@ -1,9 +1,10 @@
-import db from "../../connection.js";
+import query from "../../connection.js";
 
-const response = await db.query(
-  `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username TEXT, first_name TEXT, last_name TEXT);`
-);
+const sqlString = `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, firstName TEXT, lastName TEXT, email TEXT, address TEXT, isActive BOOL, cloudinary_id VARCHAR(128) NOT NULL, userBio TEXT);`;
 
-console.log(response);
+export async function createUsersTable() {
+   const res = await query(sqlString);
+}
+console.log(sqlString);
 
-db.end();
+createUsersTable();
