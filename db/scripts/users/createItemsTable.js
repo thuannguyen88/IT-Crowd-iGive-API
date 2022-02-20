39ac2ca031d7d5a2249f68903204d5ebe079255a
thuannguyen88 <getaholdofthuan@gmail.com>
2022-02-20 20:59:39 +0000

    set dateadded to timestamp

--- db/scripts/users/createItemsTable.js
+++ db/scripts/users/createItemsTable.js
@@ -1,6 +1,6 @@
 import query from "../../connection.js";
 
-const sqlString = `CREATE TABLE IF NOT EXISTS items (itemId SERIAL PRIMARY KEY, userId INT, category TEXT, itemName TEXT, itemDescription TEXT, useByDate DATE NOT NULL, dateAdded DATE NOT NULL, quantity INT, cloudinary_id VARCHAR(128) NOT NULL, isReserved BOOL, availability BOOL, timeslot TEXT, CONSTRAINT id_to_userId FOREIGN KEY(userId) REFERENCES users(id));`;
+const sqlString = `CREATE TABLE IF NOT EXISTS items (itemId SERIAL PRIMARY KEY, userId INT, category TEXT, itemName TEXT, itemDescription TEXT, useByDate DATE NOT NULL, dateAdded TIMESTAMP, quantity INT, cloudinary_id VARCHAR(128) NOT NULL, isReserved BOOL, availability BOOL, timeslot TEXT, CONSTRAINT id_to_userId FOREIGN KEY(userId) REFERENCES users(id));`;
 
 export async function createItemsTable() {
   const res = await query(sqlString);
