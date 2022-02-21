809e4a981a4b476a1a344d29fb8e97f005c47a01
thuannguyen88 <getaholdofthuan@gmail.com>
2022-02-21 18:11:32 +0000

    commented out port on app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(function (err, req, res, next) {
   res.status(500).json(err);
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, console.log(`Server running on port ${PORT}`));
 
 export default app;
