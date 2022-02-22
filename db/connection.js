271b885e9b4b7c40068b14a62b82542b0a3a9a71
Dmitriy Yegorov <yegorovd14@gmail.com>
2022-02-22 16:08:07 +0000

    ---

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,15 @@
 import pg from "pg";
 import { db } from "../config.js";
 
-// import { connectionString } from "../config.js";
+import { connectionString } from "../config.js";
 
 const pool = new pg.Pool({
-  // connectionString,
-  user: db.user,
-  host: db.host,
-  database: db.database,
-  password: db.password,
-  port: db.port,
+  connectionString,
+  // user: db.user,
+  // host: db.host,
+  // database: db.database,
+  // password: db.password,
+  // port: db.port,
   ssl: {
     rejectUnauthorized: false,
   },
