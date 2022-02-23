c951fb5ebea42bbbce1f1555beffc8c6b8d46283
Rory Maguire <rorymaguire00@gmail.com>
2022-02-23 09:28:01 +0000

    troubleshooting

--- routes/users.js
+++ routes/users.js
@@ -66,7 +66,7 @@ usersRouter.get("/:id", async (req, res) => {
 
 /* CREATE new user */
 usersRouter.post("/", upload.single("avatar"), async (req, res) => {
-	console.log(req.path);
+	console.log(req.file);
 });
 // 	try {
 // 		const result = await cloudinary.uploader.upload(req.file.path);
