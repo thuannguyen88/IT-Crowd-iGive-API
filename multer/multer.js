ef2fadd4091271519955a9c458ec88a427185741
Rory Maguire <rorymaguire00@gmail.com>
2022-02-23 02:06:13 +0000

    __

--- multer/multer.js
+++ multer/multer.js
@@ -2,7 +2,7 @@
 const multer = require("multer");
 const path = require("path");
 
-module.exports = multer({
+export const upload = multer({
 	storage: multer.diskStorage({}),
 	fileFilter: (req, file, cb) => {
 		let ext = path.extname(file.originalname);
