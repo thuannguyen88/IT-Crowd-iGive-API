c5c239f971ddcc8568b0fd33accff5cc955db20d
Rory Maguire <rorymaguire00@gmail.com>
2022-02-23 00:26:07 +0000

    cloudinary_url added to heroku and to back end file

--- config.js
+++ config.js
@@ -18,9 +18,11 @@ import * as Cloudinary from "cloudinary";
 const cloudinary = Cloudinary.v2;
 
 export const cloudinaryConfig = cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+	// cloud_name: process.env.CLOUDINARY_NAME,
+	// api_key: process.env.CLOUDINARY_API_KEY,
+	// api_secret: process.env.CLOUDINARY_API_SECRET,
+
+	cloudinary_url: process.env.CLOUDINARY_URL,
 	secure: true,
 });
 
