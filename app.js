aa5db705eefc114f8f7b0ce37118240b2feb0018
Rory Maguire <rorymaguire00@gmail.com>
2022-02-23 16:35:43 +0000

    express.json(limit added)

--- app.js
+++ app.js
@@ -24,7 +24,13 @@ app.get("/*", (req, res) =>
 );
 
 app.use(cors());
-app.use(express.json());
+
+app.use(
+	express.json({
+		limit: "20mb",
+	})
+);
+
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
