98c38625651065edf8dd28572cb8362b71317a50
Rory Maguire <rorymaguire00@gmail.com>
2022-02-23 10:02:16 +0000

    playing with multer config

--- multer/index.js
+++ multer/index.js
@@ -1,15 +1,5 @@
 //multer import and config
 import multer from "multer";
-import path from "path";
 
-export const upload = multer({
-	storage: multer.diskStorage({}),
-	fileFilter: (req, file, cb) => {
-		let ext = path.extname(file.originalname);
-		if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-			cb(new Error("File type is not supported"), false);
-			return;
-		}
-		cb(null, true);
-	},
-});
+const storage = multer.memoryStorage();
+export const upload = multer({ storage }).single("image");
