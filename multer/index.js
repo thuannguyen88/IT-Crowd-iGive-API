f10af4cdc5327c45e861be1bc40dcfba36567342
Rory Maguire <rorymaguire00@gmail.com>
2022-02-23 02:14:05 +0000

    little errors no commit message needed

--- multer/index.js
+++ multer/index.js
@@ -1,6 +1,6 @@
 //multer import and config
-const multer = require("multer");
-const path = require("path");
+import multer from "multer";
+import path from "path";
 
 export const upload = multer({
 	storage: multer.diskStorage({}),
