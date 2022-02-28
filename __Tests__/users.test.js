3ddec8782c32f3abaf43be9497b8ea79a1e0ddd6
irfanhussain <irfan185@gmail.com>
2022-02-28 16:24:41 +0000

    get test done

--- __Tests__/users.test.js
+++ __Tests__/users.test.js
@@ -2,18 +2,43 @@ import request from "supertest";
 import app from "../app.js";
 //import { dummyData } from "../libs/dummyData.js";
 
+//get all users
+
 describe("GET /api/users should show all users", function () {
-   test("gives us back 200 OK success status code", async function () {
-      const actual = await request(app).get("/api/users");
+  test("gives us back 200 OK success status code", async function () {
+    const actual = await request(app).get("/api/users");
 
-      // expect(actual.send).toStrictEqual(expectedSend);
-      //  expect(actual.statusCode).toBe(200);
-      expect(actual.status).toEqual(200);
-   });
+    expect(actual.status).toEqual(200);
+  });
 
-   test("gives us back an object", async function () {
-      const actual = await request(app).get("/api/users");
+  test("gives us back an object", async function () {
+    const actual = await request(app).get("/api/users");
 
-      expect(actual.body).toBeInstanceOf(Object);
-   });
+    expect(actual.body).toBeInstanceOf(Object);
+  });
 });
+
+//get a user by id
+
+describe("Get /api/users/:id", function () {
+  let id = 1;
+  test("gives us back success res 200", async function () {
+    const actual = await request(app).get(`/api/users/${id}`);
+
+    expect(actual.status).toEqual(200);
+  });
+});
+
+//create user
+
+describe("Post /api/users", function () {
+  test("gives us back success res 200", async function () {
+    const actual = await request(app).post("/api/users");
+
+    expect(actual.status).toEqual(200);
+  });
+});
+
+// edit a user
+
+//delete a user
--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,12 @@ import { uploader } from "../config.js";
 
 //import models
 import {
-	getAllUsers,
-	getUserById,
-	createUser,
-	updateUser,
-	deleteUser,
-	updateIsActiveStatus,
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  updateIsActiveStatus,
 } from "../models/users.js";
 
 //create instance of usersRouter
@@ -24,29 +24,30 @@ const usersRouter = express.Router();
 
 /* GET all users */
 usersRouter.get("/", async (req, res) => {
-	// res.send("get all users");
+  // res.send("get all users");
 
-	const users = await getAllUsers();
-	console.log(users);
-	res.json({
-		message: `all users`,
-		success: true,
-		payload: users,
-	});
+  const users = await getAllUsers();
+  console.log(users);
+
+  res.json({
+    message: `all users`,
+    success: true,
+    payload: users,
+  });
 });
 
 /* GET specific user by ID */
 usersRouter.get("/:id", async (req, res) => {
-	// res.send("get user by id");
+  // res.send("get user by id");
 
-	const id = Number(req.params.id);
-	const requestedUser = await getUserById(id);
+  const id = Number(req.params.id);
+  const requestedUser = await getUserById(id);
 
-	res.json({
-		message: `found user with id ${id}`,
-		success: true,
-		payload: requestedUser,
-	});
+  res.json({
+    message: `found user with id ${id}`,
+    success: true,
+    payload: requestedUser,
+  });
 });
 
 // /* GET specific user by EMAIL */
@@ -66,114 +67,114 @@ usersRouter.get("/:id", async (req, res) => {
 
 /* CREATE new user */
 usersRouter.post("/", async (req, res) => {
-	//extract the data from the register user form on client , sent via req.body
-	const { first_name, last_name, email, address, image, is_active, user_bio } =
-		req.body;
+  //extract the data from the register user form on client , sent via req.body
+  const { first_name, last_name, email, address, image, is_active, user_bio } =
+    req.body;
 
-	//some variables are unavailable unless scoped outside the try block
-	let result;
+  //some variables are unavailable unless scoped outside the try block
+  let result;
 
-	try {
-		//cloudinary uploader passed image which is a base 64 encoded image
-		result = await uploader.upload(image);
-	} catch (error) {
-		//if this fails, let the client know
-		console.log("upload failed", error);
-		//bad practice to return like this but ok for development
-		return;
-	}
-	//cloudinary returned us an object which we saved as const result
-	//we store the image url property as avatar
-	//we store the public_id of that image as unique cloudinary_id
-	const avatar = result.secure_url;
-	const cloudinary_id = result.public_id;
+  try {
+    //cloudinary uploader passed image which is a base 64 encoded image
+    result = await uploader.upload(image);
+  } catch (error) {
+    //if this fails, let the client know
+    console.log("upload failed", error);
+    //bad practice to return like this but ok for development
+    return;
+  }
+  //cloudinary returned us an object which we saved as const result
+  //we store the image url property as avatar
+  //we store the public_id of that image as unique cloudinary_id
+  const avatar = result.secure_url;
+  const cloudinary_id = result.public_id;
 
-	//insert these values into the users table
-	let newUser;
-	try {
-		newUser = await createUser(
-			first_name,
-			last_name,
-			email,
-			address,
-			is_active,
-			cloudinary_id,
-			avatar,
-			user_bio
-		);
-	} catch (error) {
-		console.log("create newUser failed", error);
-		return;
-	}
+  //insert these values into the users table
+  let newUser;
+  try {
+    newUser = await createUser(
+      first_name,
+      last_name,
+      email,
+      address,
+      is_active,
+      cloudinary_id,
+      avatar,
+      user_bio
+    );
+  } catch (error) {
+    console.log("create newUser failed", error);
+    return;
+  }
 
-	res.json({
-		message: `user created successfully`,
-		success: true,
-		payload: newUser,
-	});
+  res.json({
+    message: `user created successfully`,
+    success: true,
+    payload: newUser,
+  });
 });
 
 /* DELETE specific user */
 usersRouter.delete("/:id", async (req, res) => {
-	const id = Number(req.params.id);
-	const deletedUser = await deleteUser(id);
-	//also delete cloudinary id of the user we want to delete
-	const user = await getUserById(id);
-	await uploader.destroy(user.cloudinary_id);
+  const id = Number(req.params.id);
+  const deletedUser = await deleteUser(id);
+  //also delete cloudinary id of the user we want to delete
+  const user = await getUserById(id);
+  await uploader.destroy(user.cloudinary_id);
 
-	res.json({
-		message: `user successfully deleted`,
-		success: true,
-		payload: deletedUser,
-	});
+  res.json({
+    message: `user successfully deleted`,
+    success: true,
+    payload: deletedUser,
+  });
 });
 
 /* UPDATE specific user */
 usersRouter.put("/:id", async (req, res) => {
-	// res.send("user details updated successfully");
+  // res.send("user details updated successfully");
 
-	const id = Number(req.params.id);
-	const {
-		first_name,
-		last_name,
-		email,
-		address,
-		is_active,
-		cloudinary_id,
-		user_bio,
-	} = req.body;
+  const id = Number(req.params.id);
+  const {
+    first_name,
+    last_name,
+    email,
+    address,
+    is_active,
+    cloudinary_id,
+    user_bio,
+  } = req.body;
 
-	const updatedUser = await updateUser(
-		id,
-		first_name,
-		last_name,
-		email,
-		address,
-		is_active,
-		cloudinary_id,
-		user_bio
-	);
+  const updatedUser = await updateUser(
+    id,
+    first_name,
+    last_name,
+    email,
+    address,
+    is_active,
+    cloudinary_id,
+    user_bio
+  );
 
-	res.json({
-		message: `user details updated successfully`,
-		success: true,
-		payload: updatedUser,
-	});
+  res.json({
+    message: `user details updated successfully`,
+    success: true,
+    payload: updatedUser,
+  });
 });
 
 usersRouter.patch("/:id", async (req, res) => {
-	//   res.send("item reserve status updated successfully");
+  //   res.send("item reserve status updated successfully");
 
-	const id = Number(req.params.id);
-	const { is_active } = req.body;
+  const id = Number(req.params.id);
+  const { is_active } = req.body;
 
-	const userActiveStatus = await updateIsActiveStatus(id, is_active);
+  const userActiveStatus = await updateIsActiveStatus(id, is_active);
 
-	res.json({
-		message: `user active status updated successfully`,
-		success: true,
-		payload: userActiveStatus,
-	});
+  res.json({
+    message: `user active status updated successfully`,
+    success: true,
+    payload: userActiveStatus,
+  });
 });
 
 // =-=-=-=-=-=-=-=-=-=-=
