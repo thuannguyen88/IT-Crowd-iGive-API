906915d86a3d7cd6e4b4c26de188cc683b7e7a50
thuannguyen88 <getaholdofthuan@gmail.com>
2022-03-01 11:41:26 +0000

    refactor merge

--- __Tests__/users.test.js
+++ __Tests__/users.test.js
@@ -1,144 +1,112 @@
 import request from "supertest";
 import app from "../app.js";
-import { jest } from "@jestGlobals";
+import { uploader } from "../config.js";
+import { jest } from "@jest/globals";
+import fs from "fs";
 
 //import { dummyData } from "../libs/dummyData.js";
 
 //get all users
 
 // describe("GET /api/users should show all users", function () {
-//    test("gives us back 200 OK success status code", async function () {
-//       const actual = await request(app).get("/api/users");
+//   test("gives us back 200 OK success status code", async function () {
+//     const actual = await request(app).get("/api/users");
 
-//       expect(actual.status).toEqual(200);
-//    });
+//     expect(actual.status).toEqual(200);
+//   });
 
-//    test("gives us back an object", async function () {
-//       const actual = await request(app).get("/api/users");
+//   test("gives us back an object", async function () {
+//     const actual = await request(app).get("/api/users");
 
-//       expect(actual.body).toBeInstanceOf(Object);
-//    });
+//     expect(actual.body).toBeInstanceOf(Object);
+//   });
 // });
+
+describe("GET /api/users", function () {
+  test("it should give us back 200 SUCCESS", async function () {
+    const actual = await request(app)
+      .get("/api/users")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    //  expect(actual.status).toEqual(200);
+  });
+
+  test("it should give us back an object", async function () {
+    const actual = await request(app).get("/api/users");
+    expect(actual.body).toBeInstanceOf(Object);
+  });
+});
+
+describe("Get /api/users/:id", function () {
+  let id = 1;
+  test("it should give us back 200 SUCCESS", async function () {
+    const actual = await request(app)
+      .get(`/api/users/${id}`)
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    //  expect(actual.status).toEqual(200);
+  });
+});
+
+
+// implementing mock for GET to /api/users
 describe("GET /api/users should show all users", function () {
-   test("gives us back all users", () => {
-      const spy = jest.spyOn(users, "get all users");
-      const getUsers = users.getAll();
+  test("gives us back all users", () => {
+    const spy = jest.spyOn(users, "get all users");
+    const getUsers = users.getAll();
 
-      expect(spy).toHaveBeenCalled();
-      expect(getUsers).toBe(true);
-      import { uploader } from "../config.js";
-      import { jest } from "@jest/globals";
-      import fs from "fs";
+    expect(spy).toHaveBeenCalled();
+    expect(getUsers).toBe(true);
+    spy.mockRestore();
 
-      //get all users
+    //get all users
+  });
+});
 
-      describe("GET /api/users", function () {
-         test("it should give us back 200 SUCCESS", async function () {
-            const actual = await request(app)
-               .get("/api/users")
-               .expect("Content-Type", /json/)
-               .expect(200);
 
-            //  expect(actual.status).toEqual(200);
-         });
+// implementing mock for POST to /api/users
+describe("Post /api/users", function () {
+  test("it should give us back 201 CREATED", async function () {
+    // create a mock upload function for cloudinary
+    const mockUploader = jest.spyOn(uploader, "upload");
 
-         test("it should give us back an object", async function () {
-            const actual = await request(app).get("/api/users");
+    // create a mock function for creating user
+    const createUser = jest.fn();
 
-            spy.mockRestore();
-         });
-      });
+    // using dummy image to pass through to upload()
+    const image = fs.readFileSync("./avatar.jpeg").toString("base64");
 
-      //get a user by id
+    // using cloudinary upload(image)
+    const { secure_url, public_id } = await uploader.upload(image);
 
-      // describe("Get /api/users/:id", function () {
-      //    let id = 1;
-      //    test("gives us back success res 200", async function () {
-      //       const actual = await request(app).get(`/api/users/${id}/`);
+    const avatar = secure_url;
+    const cloudinary_id = public_id;
 
-      //       expect(actual.status).toEqual(200);
-      //    });
-      // });
-      describe("Get /api/users/:id", function () {
-         let id = 1;
-         test("it should give us back 200 SUCCESS", async function () {
-            const actual = await request(app)
-               .get(`/api/users/${id}`)
-               .expect("Content-Type", /json/)
-               .expect(200);
+    createUser.mockReset();
 
-            //  expect(actual.status).toEqual(200);
-         });
-      });
+    const actual = await request(app).post("/api/users").send({
+      first_name: "Graham",
+      last_name: "Delbeck",
+      email: "graham.delbeck@gmail.com",
+      address: "Main Street, LA22 9BU, Ambleside, United Kingdom",
+      //   image: `${image}`,
+      is_active: true,
+      cloudinary_id: cloudinary_id,
+      avatar: avatar,
+      user_bio: "hello I'm another Graham",
+    });
+    expect(createUser.mock.calls.length).toBe(1);
 
-      // //create user
+    // test("It should expect a json and a success create status code of 201")
+    // .expect("Content-Type", /json/)
+    // .expect(201);
 
-      // describe("Post /api/users", function () {
-      //    test("gives us back success res 200", async function () {
-      //       const actual = await request(app).post("/api/users");
+    // test("it should upload image successfully to cloudinary")
+    // test("it should create new user")
+    // test("It should respond with an object containing; message, success and payload ")
 
-      //       expect(actual.status).toEqual(200);
-      //    });
-      // });
-
-      // // edit a user
-
-      // describe("Patch /api/users", function () {
-      //    test("gives us back success res 200", async function () {
-      //       const actual = await request(app).post("/api/users");
-      describe("Post /api/users", function () {
-         test("it should give us back 201 CREATED", async function () {
-            const mockUploader = jest.spyOn(uploader, "upload");
-            const createUser = jest.fn();
-            const image = fs.readFileSync("./avatar.jpeg").toString("base64");
-            const isUploaded = await uploader.upload(image);
-
-            createUser.mockReset();
-            const actual = await request(app)
-               .post("/api/users")
-               .send({
-                  first_name: "Graham",
-                  last_name: "Delbeck",
-                  email: "graham.delbeck@gmail.com",
-                  address: "Main Street, LA22 9BU, Ambleside, United Kingdom",
-                  image: `${image}`,
-                  is_active: true,
-                  // cloudinary_id: "",
-                  // avatar:
-                  //   "https://cdn.vox-cdn.com/thumbor/00awoM5IS2kFITs9546UyMSePBY=/0x0:2370x1574/1200x800/filters:focal(996x598:1374x976)/cdn.vox-cdn.com/uploads/chorus_image/image/69715362/Screen_Shot_2020_07_21_at_9.38.25_AM.0.png",
-                  user_bio: "hello I'm another Graham",
-               });
-            expect(createUser.mock.calls.length).toBe(1);
-
-            // .expect("Content-Type", /json/)
-            // .expect(201);
-
-            // test("it should upload image successfully to cloudinary")
-            // test("it should create new user")
-
-            //  expect(actual.status).toEqual(201);
-         });
-      });
-
-      // .expect("Content-Type", /json/)
-      // .expect(201);
-
-      // test("it should upload image successfully to cloudinary")
-      // test("it should create new user")
-
-      //  expect(actual.status).toEqual(201);
-      // });
-      //});
-
-      //       expect(actual.status).toEqual(200);
-      //    });
-      // });
-
-      // //delete a user
-      //delete a user
-
-      //jest.fn()
-      //jest.spyOn()
-      //jest.mock()
-   });
+    expect(actual.status).toEqual(201);
+  });
 });
