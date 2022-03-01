874091e3795c84adf11a413a8b08158eb1891e02
Simren Chandan <simrenkaursoor@gmail.com>
2022-03-01 10:25:53 +0000

    added mock function test for get route

--- __Tests__/users.test.js
+++ __Tests__/users.test.js
@@ -1,44 +1,65 @@
 import request from "supertest";
 import app from "../app.js";
+import { jest } from "@jestGlobals";
+
 //import { dummyData } from "../libs/dummyData.js";
 
 //get all users
 
+// describe("GET /api/users should show all users", function () {
+//    test("gives us back 200 OK success status code", async function () {
+//       const actual = await request(app).get("/api/users");
+
+//       expect(actual.status).toEqual(200);
+//    });
+
+//    test("gives us back an object", async function () {
+//       const actual = await request(app).get("/api/users");
+
+//       expect(actual.body).toBeInstanceOf(Object);
+//    });
+// });
 describe("GET /api/users should show all users", function () {
-  test("gives us back 200 OK success status code", async function () {
-    const actual = await request(app).get("/api/users");
+   test("gives us back all users", () => {
+      const spy = jest.spyOn(users, "get all users");
+      const getUsers = users.getAll();
 
-    expect(actual.status).toEqual(200);
-  });
+      expect(spy).toHaveBeenCalled();
+      expect(getUsers).toBe(true);
 
-  test("gives us back an object", async function () {
-    const actual = await request(app).get("/api/users");
-
-    expect(actual.body).toBeInstanceOf(Object);
-  });
+      spy.mockRestore();
+   });
 });
 
 //get a user by id
 
-describe("Get /api/users/:id", function () {
-  let id = 1;
-  test("gives us back success res 200", async function () {
-    const actual = await request(app).get(`/api/users/${id}`);
+// describe("Get /api/users/:id", function () {
+//    let id = 1;
+//    test("gives us back success res 200", async function () {
+//       const actual = await request(app).get(`/api/users/${id}/`);
 
-    expect(actual.status).toEqual(200);
-  });
-});
+//       expect(actual.status).toEqual(200);
+//    });
+// });
 
-//create user
+// //create user
 
-describe("Post /api/users", function () {
-  test("gives us back success res 200", async function () {
-    const actual = await request(app).post("/api/users");
+// describe("Post /api/users", function () {
+//    test("gives us back success res 200", async function () {
+//       const actual = await request(app).post("/api/users");
 
-    expect(actual.status).toEqual(200);
-  });
-});
+//       expect(actual.status).toEqual(200);
+//    });
+// });
 
-// edit a user
+// // edit a user
 
-//delete a user
+// describe("Patch /api/users", function () {
+//    test("gives us back success res 200", async function () {
+//       const actual = await request(app).post("/api/users");
+
+//       expect(actual.status).toEqual(200);
+//    });
+// });
+
+// //delete a user
